dbd812a04a2358888a120f0ec692ad21336fcc68
jingsq <jingsiqi@xiniaoyun.com>
2019-12-11 18:23:39 +0800

    。。

---
+++ src/main/java/com/jsq/demo/common/utils/mybasedao/MyBaseSqlProvider.java
@@ -0,0 +1,11 @@
+package com.jsq.demo.common.utils.mybasedao;
+
+/**
+ * @author Administrator
+ */
+public class MyBaseSqlProvider {
+
+    public String findOne() {
+        return null;
+    }
+}
